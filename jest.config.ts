fe89a618d685a126a28a9a1b285955d8a7941f4b
Camila Scaglioni <camila.pia.scaglioni@gmail.com>
2025-10-13 13:08:22 -0300

    Tests para la funcionalidad de registro: RegisterTest Configuraciones para correr los tests: jest.config.ts, setupTests.ts, types.d.ts, ts.config.json, package.json, package-lock.json Modificaciones en css de componentes Header, ModelCard, SearchBar y App

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }]
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/']
+}
+
+export default config
